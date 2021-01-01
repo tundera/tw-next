cfdc22960fe6cc1da3b4a4780afed6042bc22000
tundera <stackshuffle@gmail.com>
2021-01-01 11:53:31 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const { merge } = require("merge-anything");
+
+// Base config from https://www.tailwind-kit.com/started
+const base = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    // These options are passed through directly to PurgeCSS
+  },
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
+
+const config = {};
+
+module.exports = merge(base, config);
