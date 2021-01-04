e97411e5d0a705ff30a7252be6f41b6fe548c160
tundera <stackshuffle@gmail.com>
2021-01-03 22:47:59 -0700

    progress on dark mode

---
+++ src/components/FormSubscribe.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+import InputText from "./InputText";
+
+interface Props {
+  label: string;
+  placeholder: string;
+}
+
+const FormSubscribe: FC<Props> = ({ label, placeholder }) => {
+  return (
+    <form className="flex w-full max-w-sm space-x-3">
+      <InputText placeholder={placeholder} />
+      <button
+        className="flex-shrink-0 bg-purple-600 text-white text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+        type="submit"
+      >
+        {label}
+      </button>
+    </form>
+  );
+};
+export default FormSubscribe;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,54 @@ import type { AppProps /*, AppContext */ } from "next/app";
 import { GlobalStyles } from "twin.macro";
 
 import Header from "@components/Header";
+import "@styles/globals.css";
+
+const links = [
+  {
+    label: "Home",
+    isSelected: true,
+    link: "/",
+    desc: "Home Page",
+  },
+  {
+    label: "Gallery",
+    isSelected: false,
+    link: "/gallery",
+    desc: "Gallery Page",
+  },
+  {
+    label: "Content",
+    isSelected: false,
+    link: "/content",
+    desc: "Content Page",
+  },
+  {
+    label: "Contact",
+    isSelected: false,
+    link: "/contact",
+    desc: "Contact Page",
+  },
+];
+const ddmItems = [
+  {
+    label: "Settings",
+  },
+  {
+    label: "Account",
+  },
+  {
+    label: "Logout",
+  },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <GlobalStyles />
-      <Header />
-      <Component {...pageProps} />
+      <div className="bg-white dark:bg-black">
+        <Header links={links} ddmItems={ddmItems} />
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
